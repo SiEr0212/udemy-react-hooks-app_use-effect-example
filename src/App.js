c97a487de64e326436378d7ebdb964448f18226d
SiEr0212 <simonertel@gmx.de>
2021-05-03 22:23:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(Date);
+
+  useEffect(() => {
+    let handle = setInterval(() => {
+      setTime(Date);
+    }, 1000);
+
+    return () => {
+      clearInterval(handle);
+    };
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Use Effect Examples</h2>
+        <h3>Date & Time: {time}</h3>
       </header>
     </div>
   );
