4805a506d51e0bfd826d6449a6b2c556ebfbcf12
SiEr0212 <simonertel@gmx.de>
2021-05-03 22:40:47 +0200

    2ndt commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const initXY = {
+  x: null,
+  y: null,
+};
+
 function App() {
   const [time, setTime] = useState(Date);
+  const [xy, setXy] = useState(initXY);
 
   useEffect(() => {
     let handle = setInterval(() => {
@@ -13,11 +19,27 @@ function App() {
       clearInterval(handle);
     };
   });
+
+  const mousemoveHandle = (e) => {
+    setXy({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", mousemoveHandle);
+    return () => {
+      window.removeEventListener('mousemove', mousemoveHandle)
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>Use Effect Examples</h2>
         <h3>Date & Time: {time}</h3>
+        <h3>{`x:${xy.x}, y:${xy.y}`}</h3>
       </header>
     </div>
   );
